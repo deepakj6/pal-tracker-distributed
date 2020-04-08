7839e3ed9f63835a47b95cef58b280644023af08
deepakj6 <deepakj6@yahoo.co.in>
2020-04-08 12:06:28 +0000

    VILT-1

--- Applications/BacklogServer/Startup.cs
+++ Applications/BacklogServer/Startup.cs
@@ -20,6 +20,7 @@ namespace BacklogServer
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        //HI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddLogging(loggingBuilder =>
